460b6231fd8a16a48fbf57b42aa7d6a250b05da5
ageng <newbie.masih01@gmail.com>
2014-10-12 05:49:42 +0700

    update javascript

--- js/main.js
+++ js/main.js
@@ -10,6 +10,47 @@ $(document).ready(function () {
 		if (encType === 'e') {
 
 			var getHash = location.hash.substr(2);
-		}
+            $('#write-text').val(Base64.decode(getHash));
+            $('#share-url').val(location.href);
+            $('#share').show();
+            $('#write-text').trigger('autosize');
+		} else if (encType === 'b') {
+
+            var getHash = location.hash.substr(2);
+            $('#write-text').val("Pesan ini dienkripsi. Massukan key untuk dekrip pesan ini.");
+
+            $('#key-required').show();
+            $('#decrypt-button').show();
+            $('#save-button').hide();
+            $('#add-key-button').hide();
+        }
 	}
+
+    $('#save-button').click(function (e) {
+        var text = $('#write-text').val(),
+            encoded = Base64.encode(text);
+        key = $('#key-gen').val();
+
+        if (text === '') {
+            $('alert').show().text("").delay(3000).fadeOut('slow');
+            e.preventDefault();
+        } else if (key.length === 0) {
+
+            location.hash = 'e' + encoded;
+            $('#share-url').val(location.href);
+            $('#share').slideDown('fast');
+        } else {
+
+            var bf = new Blowfish({
+                key: key,
+            });
+            var encrypted = bf.encrypted(text);
+
+            location.hash = 'b' + encrypted;
+            $('#share-url').val('location.href');
+            $('#share').slideDown('fast');
+        }
+    });
+
+
 })
